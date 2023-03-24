521737d9991f1bbdaa431f91f67008be442193e9
muazzam <muazzamafaque@gmail.com>
2023-03-25 01:02:56 +0500

    done without comments, now only have to implement the feature of showing downloaded files within app

--- lib/download_video_screen.dart
+++ lib/download_video_screen.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
-import 'package:android_path_provider/android_path_provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:you_down/utils/main_utils.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:you_down/model/video_model.dart';
@@ -248,81 +245,6 @@ class _DownloadVideoScreenState extends State<DownloadVideoScreen> {
 
     print('external path: $_localPath');
   }
-
-  // Future<List<String?>> _getSavedDir() async {
-  //   String? externalStorageDirPath;
-  //   String? musicDirPath;
-  //   String? videoDirPath;
-
-  //   if (Platform.isAndroid) {
-  //     try {
-  //       final androidInfo = await deviceInfoPlugin.androidInfo;
-  //       if (androidInfo.version.sdkInt > 29) {
-  //         final directories = await getExternalStorageDirectories();
-
-  //         print(directories);
-  //         final dir = await getExternalStorageDirectory();
-
-  //         externalStorageDirPath = dir?.path;
-  //       } else {
-  //         externalStorageDirPath = await AndroidPathProvider.downloadsPath;
-  //       }
-  //       musicDirPath = await AndroidPathProvider.musicPath;
-  //       videoDirPath = await AndroidPathProvider.moviesPath;
-  //     } catch (err, st) {
-  //       print('failed to get downloads path: $err, $st');
-
-  //       final dir = await getApplicationDocumentsDirectory();
-
-  //       externalStorageDirPath = dir.path;
-  //     }
-  //   } else if (Platform.isIOS) {
-  //     externalStorageDirPath =
-  //         (await getApplicationDocumentsDirectory()).absolute.path;
-  //   }
-
-  //   return [
-  //     "$externalStorageDirPath/$appName",
-  //     "$musicDirPath/$appName",
-  //     "$videoDirPath/$appName"
-  //   ];
-  // }
-
-  // Future<bool> _checkPermission() async {
-  //   if (Platform.isIOS) {
-  //     return true;
-  //   }
-
-  //   if (Platform.isAndroid) {
-  //     final androidInfo = await deviceInfoPlugin.androidInfo;
-  //     print("sdk int: ${androidInfo.version.sdkInt}");
-
-  //     if (androidInfo.version.sdkInt > 29) {
-  //       final externalStorageStatus =
-  //           await Permission.manageExternalStorage.status;
-
-  //       if (externalStorageStatus == PermissionStatus.granted) {
-  //         return true;
-  //       } else {
-  //         final externalStorageResult =
-  //             await Permission.manageExternalStorage.request();
-
-  //         return externalStorageResult == PermissionStatus.granted;
-  //       }
-  //     } else {
-  //       final storageStatus = await Permission.storage.status;
-
-  //       if (storageStatus == PermissionStatus.granted) {
-  //         return true;
-  //       } else {
-  //         final storageResult = await Permission.storage.request();
-  //         return storageResult == PermissionStatus.granted;
-  //       }
-  //     }
-  //   }
-
-  //   throw StateError('unknown platform');
-  // }
 
   //TODO: move this to controller file if state management created.
   Future<void> downloadFile(StreamInfo stream, VideoModel video) async {
